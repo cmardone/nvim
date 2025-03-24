887c308b81ff77ce47eb7d0ca1356f6fc6d906c9
Cristóbal Mardones Bucarey <cmardone@gmail.com>
2025-03-24 17:58:29 -0300

    Finish completions setup

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -3,7 +3,8 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
-    options = function()
+    config = function()
+      -- Install ripgrep for better experience
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
